4b928abf8a70abb55a0dd26d2ff93b291fb7bd77
willjantscher <william.jantscher@gmail.com>
2020-11-19 09:02:58 -0700

    stuff

---
+++ students/src/App.js
@@ -0,0 +1,37 @@
+//Student Microservice app
+//Will Jantscher
+//https://github.com/willjantscher/SDI_Project_1_Students
+//the react front end runs on localhost:6001
+
+//npm install
+//npm install express --save
+
+//npm run start
+
+
+import './App.css';
+import React from 'react'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Student
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
